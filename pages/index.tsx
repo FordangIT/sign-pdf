ccd730b09593d240ef3d1a8805efa1dfcf4be0b3
FordangIT <fordang0819@gmail.com>
2023-04-30 12:45:27 +0900

    [FE]기본세팅 완료

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+const HomePage: FC = (): JSX.Element => {
+  return (
+    <>
+      <h1>pdf 파일을 선택하세요</h1>
+      <div>파일 찾아보기</div>
+    </>
+  );
+};
+export default HomePage;
